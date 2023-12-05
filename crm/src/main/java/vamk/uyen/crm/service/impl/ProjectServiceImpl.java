e4ebf34e12940b38fff2e37d6a29117fc4c9dd51
e2000592 <e2000592@vamk.fi>
2023-12-05 20:36:05 +0200

    Modify update project feature to have validation date

--- crm/src/main/java/vamk/uyen/crm/service/impl/ProjectServiceImpl.java
+++ crm/src/main/java/vamk/uyen/crm/service/impl/ProjectServiceImpl.java
@@ -8,6 +8,11 @@ import vamk.uyen.crm.dto.response.ProjectResponse;
 import vamk.uyen.crm.entity.ProjectEntity;
 import vamk.uyen.crm.repository.ProjectRepository;
 import vamk.uyen.crm.service.ProjectService;
+import vamk.uyen.crm.util.DateVadilationUtil;
+
+import static vamk.uyen.crm.util.DateVadilationUtil.validateDate;
+
+import java.time.LocalDate;
 
 @Service
 @RequiredArgsConstructor
@@ -16,6 +21,14 @@ public class ProjectServiceImpl implements ProjectService {
 
     @Override
     public ProjectResponse addProject(ProjectRequest projectRequest) {
+        if(projectRequest.getStartDate() != null && projectRequest.getEndDate() != null){
+            LocalDate startDate = LocalDate.parse(projectRequest.getStartDate());
+            LocalDate endDate = LocalDate.parse(projectRequest.getEndDate());
+
+            if(endDate.isBefore(startDate)){
+                throw new IllegalArgumentException("End date must be later than start date");
+            }
+        }
         // convert to entity
         ProjectEntity projectEntity = Converter.toModel(projectRequest, ProjectEntity.class);
         // save database
@@ -31,17 +44,19 @@ public class ProjectServiceImpl implements ProjectService {
         //if no update entity was found then return null
         if(updateProjectEntity == null){
             return null;
+        }else{
+            //if the update entity is exist then assign the update entity fields with the field of the request entity
+            updateProjectEntity.setName(projectEntity.getName());
+            updateProjectEntity.setStartDate(projectEntity.getStartDate());
+            updateProjectEntity.setEndDate(projectEntity.getEndDate());
+
+            //then save the update entity to database
+            projectEntity = projectRepository.save(updateProjectEntity);
+
+            //convert and return the update entity
+            return Converter.toModel(projectEntity, ProjectResponse.class);
         }
-        //if the update entity is exist then assign the update entity fields with the field of the request entity
-        updateProjectEntity.setName(projectEntity.getName());
-        updateProjectEntity.setStartDate(projectEntity.getStartDate());
-        updateProjectEntity.setEndDate(projectEntity.getEndDate());
 
-        //then save the update entity to database
-        projectEntity = projectRepository.save(updateProjectEntity);
-
-        //convert and return the update entity
-        return Converter.toModel(projectEntity, ProjectResponse.class);
     }
 
     @Override
