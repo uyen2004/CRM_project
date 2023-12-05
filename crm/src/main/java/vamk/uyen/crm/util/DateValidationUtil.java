6666a2e56554d2f618c67595a34781b979a6ef7b
e2000592 <e2000592@vamk.fi>
2023-12-05 22:16:31 +0200

    Fix DateValidationUtil class error

--- crm/src/main/java/vamk/uyen/crm/util/DateValidationUtil.java
+++ crm/src/main/java/vamk/uyen/crm/util/DateValidationUtil.java
@@ -5,13 +5,13 @@ import java.time.format.DateTimeFormatter;
 
 public class DateValidationUtil {
     public static boolean isDateValidate(String startDate, String endDate) {
-            LocalDate parsedStartDate = LocalDate.parse(startDate, DateTimeFormatter.ISO_DATE);
-            LocalDate parsedEndDate = LocalDate.parse(endDate, DateTimeFormatter.ISO_DATE);
+        LocalDate parsedStartDate = LocalDate.parse(startDate, DateTimeFormatter.ISO_DATE);
+        LocalDate parsedEndDate = LocalDate.parse(endDate, DateTimeFormatter.ISO_DATE);
 
-            if (parsedEndDate.isBefore(parsedStartDate)) {
-                return false;
-            }else{
-                return true;
-            }
+        if (parsedEndDate.isBefore(parsedStartDate)) {
+            return false;
+        }else{
+            return true;
         }
+    }
 }
